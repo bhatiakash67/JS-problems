bbd0b1cf59972c2e174991b40c81dc6e1a71649c
bhatiakash67 <akashbhati67@gmail.com>
2024-01-29 00:18:45 +0530

    webpage animation using slice and setInterval

--- 5stringmanipulation.js
+++ 5stringmanipulation.js
@@ -14,14 +14,27 @@ let rotatedString = rotateString(word, rotatingAmount)
 console.log(`The string is : ${word}. The rotated string after ${rotatingAmount} position right rotation is: ${rotatedString}`)
 
 
-// string animation using slice and setInterval method
+// string animation on webpage using slice and setInterval method
 
 const animation = document.getElementById("rotatingString")
+const userInput = document.getElementById("string")
+const btn = document.getElementById("btn")
+
 
 const changeString = (word) => {
     return word.slice(-1) + word.slice(0, -1)
 }
 
+btn.addEventListener("click", () => {
+    const userValue = userInput.value.trim()
+    if(userValue){
+        animation.textContent = userValue
+    }
+    else{
+        alert("Please enter a string value before clicking on enter")
+    }
+})
+
 setInterval(() => {
     animation.textContent = changeString(animation.textContent)
-}, 500)
+}, 600)
