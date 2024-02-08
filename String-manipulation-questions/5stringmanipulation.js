cf9e4ef5db04341dc69855b24dc78ed9c960e3bd
bhatiakash67 <akashbhati67@gmail.com>
2024-02-08 22:34:03 +0530

    this keyword and string manipulation problem

--- String-manipulation-questions/5stringmanipulation.js
+++ String-manipulation-questions/5stringmanipulation.js
@@ -1,40 +1,56 @@
-//6 .Program for right rotation of a string
+// //6 .Program for right rotation of a string
 
-const words = ["NOTEBOOK", "TEXTBOOK", "TEACHER", "SCHOOL"];
-let word = words[Math.floor(Math.random() * words.length)]
-let rotatingAmount = Math.floor(Math.random() * 10) + 1
+// const words = ["NOTEBOOK", "TEXTBOOK", "TEACHER", "SCHOOL"];
+// let word = words[Math.floor(Math.random() * words.length)]
+// let rotatingAmount = Math.floor(Math.random() * 10) + 1
 
-const rotateString = (word, rotatingAmount) => {
-    rotatingAmount %= word.length
-    return word.slice(-rotatingAmount) + word.slice(0, -rotatingAmount)
-}
+// const rotateString = (word, rotatingAmount) => {
+//     rotatingAmount %= word.length
+//     return word.slice(-rotatingAmount) + word.slice(0, -rotatingAmount)
+// }
 
-let rotatedString = rotateString(word, rotatingAmount)
+// let rotatedString = rotateString(word, rotatingAmount)
 
-console.log(`The string is : ${word}. The rotated string after ${rotatingAmount} position right rotation is: ${rotatedString}`)
-
-
-// string animation on webpage using slice and setInterval method
-
-const animation = document.getElementById("rotatingString")
-const userInput = document.getElementById("string")
-const btn = document.getElementById("btn")
+// console.log(`The string is : ${word}. The rotated string after ${rotatingAmount} position right rotation is: ${rotatedString}`)
 
 
-const changeString = (word) => {
-    return word.slice(-1) + word.slice(0, -1)
-}
+// // string animation on webpage using slice and setInterval method
 
-btn.addEventListener("click", () => {
-    const userValue = userInput.value.trim()
-    if(userValue){
-        animation.textContent = userValue
-    }
-    else{
-        alert("Please enter a string value before clicking on enter")
-    }
-})
+// const animation = document.getElementById("rotatingString")
+// const userInput = document.getElementById("string")
+// const btn = document.getElementById("btn")
 
-setInterval(() => {
-    animation.textContent = changeString(animation.textContent)
-}, 600)
+
+// const changeString = (word) => {
+//     return word.slice(-1) + word.slice(0, -1)
+// }
+
+// btn.addEventListener("click", () => {
+//     const userValue = userInput.value.trim()
+//     if(userValue){
+//         animation.textContent = userValue
+//     }
+//     else{
+//         alert("Please enter a string value before clicking on enter")
+//     }
+// })
+
+// setInterval(() => {
+//     animation.textContent = changeString(animation.textContent)
+// }, 600)
+
+// 13. To get the filename extension
+
+const file = "groot.js"
+
+ let filetype = file.split(".").pop()
+console.log(filetype)
+
+//14. Create a variable using user-defined name.
+
+const red = "number"
+const blue = 55
+
+this[red] = blue
+
+console.log(this[red])
